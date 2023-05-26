92b768ac7f55e21bcd4c841bbaf78b479a19a5e0
Ahmedsherif74 <ahmedsherifsaid2000@gmail.com>
2023-05-26 18:20:30 +0300

    Validation done

--- Milestone 3/js/LoginScript.js
+++ Milestone 3/js/LoginScript.js
@@ -12,7 +12,19 @@ document.getElementById("login-button").addEventListener("click", function(event
         window.location.href = "SystemAdminOverview.html"; // Redirect to success page for user2
       } else if (username === "ClientUserName" && password === "ClientPassword") {
         window.location.href = "ClientOverview.html"; // Redirect to success page for user3 
-      }else {
+      }
+      if (username === "" || password === "") {
+        swal({
+          title: "Please Enter Your Credentials",
+          text: "Both email and password are required",
+          icon: "error",
+          button: {
+            text: "OK",
+            className: "btn-black",
+          },
+          closeOnClickOutside: false,
+        });}
+        else {
         swal({
           title: "Invalid Credentials",
           text: "Please try again",
