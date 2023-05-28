ce316090bc1c1d592510f83d8083c10bc7e7126a
AbdelrahmanRewaished <abdelrahmann.salehh@gmail.com>
2023-05-28 21:29:38 +0300

    Done with client loans view

--- Milestone 3/js/ClientLoanScript.js
+++ Milestone 3/js/ClientLoanScript.js
@@ -1,15 +1,32 @@
 function payInstallement() {
     const selectItem = document.getElementById("exampleFormControlSelect1");
     if(selectItem.selectedIndex === 0) {
+        // 
+       
         document.getElementById("billAmount").innerHTML = "0";
-        document.getElementById("payBillFullyButton").disabled = true;
+        document.getElementById("pay-installement").disabled = true;
         const paidInstallementsDetail = document.getElementById("paid-installements");
         paidInstallementsDetail.textContent = Number(paidInstallementsDetail.textContent) + 1;
         const remainingInstallementsDetail = document.getElementById("remaining-installements")
         remainingInstallementsDetail.textContent = Number(remainingInstallementsDetail.textContent) - 1;
+         // Get the modal element
+         const modal = document.getElementById("success-payment");
+
+         // Initialize the modal using the Bootstrap JavaScript library
+         const myModal = new bootstrap.Modal(modal);
+     
+         // Show the modal using modal.show()
+         myModal.show();
     }
     else {
+         // Get the modal element
+         const modal = document.getElementById("failing-payment");
 
+         // Initialize the modal using the Bootstrap JavaScript library
+         const myModal = new bootstrap.Modal(modal);
+     
+         // Show the modal using modal.show()
+         myModal.show();
     }
     
 }
