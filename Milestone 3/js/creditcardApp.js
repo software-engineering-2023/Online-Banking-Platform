642f88ebca62168690fc065770d0929e9781515e
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2023-05-28 22:04:03 +0300

    fixed html import errors and added data reference

--- Milestone 3/js/creditcardApp.js
+++ Milestone 3/js/creditcardApp.js
@@ -1,9 +1,11 @@
 document.getElementById('applicationModal').addEventListener('show.bs.modal', function (event) {
     // Get the button that triggered the modal
     var button = event.relatedTarget;
+    var id = +button.id.substring(1);
+
 
     // Get the application details from the button's data attributes or other sources
-    var appName = "John Doe"; // Example application name
+    var appName = (clientIDs[id] == 4763380)?"AbuElKheir": "Andrew Mostafa Tate"; // Example application name
     var appEmploymentStatus = "Employed"; // Example employment status
     var appDate = '2023/10/7 23:10';
     var empID = 4763380;
@@ -16,7 +18,41 @@ document.getElementById('applicationModal').addEventListener('show.bs.modal', fu
     // Add more code to populate other application details
 });
 
+// document.addEventListener("DOMContentLoaded", function() {
+//     var row = document.getElementById('rowToHide');
+//     row.style.display = 'none';
+//   });
+  
+$(document).ready(function(){
+    $("button").click(function(){
+alert("The button was clicked." + clientIDs[0]);
+        var buttonId = $(this).attr("id");
+        // alert("Clicked button ID: " + buttonId);
+            $("#aa").hide();
+    });
+  });
+
+
 
 function openPDF() {
     window.open('assets/Employment.pdf', '_blank');
   }
+
+
+function acceptOrRejectApp(rowid){
+    
+    //hide the row
+    //alert("Application Accepted" + "  row" + rowid);
+    console.log('row0');
+    var row = document.getElementById("anate3bt");
+    row.textContent = "Offff b2a";
+    // Hide the row by setting its display property to 'none'
+    row.style.display = 'none';
+}
+function test() {
+    var lst = document.getElementsByClassName("myrow");
+    console.log(lst.length);
+    for(var i = 0; i < lst.length; ++i) {
+        lst[i].style.display = 'none';
+    }
+}
