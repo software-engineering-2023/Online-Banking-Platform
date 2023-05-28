7400c572567efe70493cb46cc0f1d3ea6cfa7e17
AbdelrahmanRewaished <abdelrahmann.salehh@gmail.com>
2023-05-28 20:04:58 +0300

    Committing to switch to a branch :|

--- Milestone 3/js/ClientLoanScript.js
+++ Milestone 3/js/ClientLoanScript.js
@@ -1,8 +1,15 @@
-function payBillFully() {
-    document.getElementById("billAmount").innerHTML = "0";
-    document.getElementById("payBillFullyButton").disabled = true;
-    const paidInstallementsDetail = document.getElementById("paid-installements");
-    paidInstallementsDetail.textContent = Number(paidInstallementsDetail.textContent) + 1;
-    const remainingInstallementsDetail = document.getElementById("remaining-installements")
-    remainingInstallementsDetail.textContent = Number(remainingInstallementsDetail.textContent) - 1;
+function payInstallement() {
+    const selectItem = document.getElementById("exampleFormControlSelect1");
+    if(selectItem.selectedIndex === 0) {
+        document.getElementById("billAmount").innerHTML = "0";
+        document.getElementById("payBillFullyButton").disabled = true;
+        const paidInstallementsDetail = document.getElementById("paid-installements");
+        paidInstallementsDetail.textContent = Number(paidInstallementsDetail.textContent) + 1;
+        const remainingInstallementsDetail = document.getElementById("remaining-installements")
+        remainingInstallementsDetail.textContent = Number(remainingInstallementsDetail.textContent) - 1;
+    }
+    else {
+
+    }
+    
 }
