7d3417fe22d4a8919db486a6b485c1da687f3851
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2023-05-29 20:22:26 +0300

    fixed logout position

--- Milestone 3/js/BankerApplicationsScript.js
+++ Milestone 3/js/BankerApplicationsScript.js
@@ -140,6 +140,27 @@ function openPDF() {
   }
 
 
+
+  const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+  const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+  
+    alertPlaceholder.append(wrapper)
+  }
+  
+  const alertTrigger = document.getElementById('liveAlertBtn')
+  if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+      appendAlert('Nice, you triggered this alert message!', 'success')
+    })
+  }
+
 // function acceptOrRejectApp(rowid){
     
 //     //hide the row
